c06f93bc532fe79bf18486efe8bde40a8275b272
Medini Patil <patilmedini333@gmail.com>
2025-11-16 13:01:57 -0800

    "Requirements Added"

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="InnerHarmony_chatbot",
+    version="0.1.0",
+    author="Medini",
+    author_email="patilmedini333@gmail.com",
+    packages=find_packages();
+    install requires=[]
+)
